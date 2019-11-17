9dcf7cc85e544912dc2b98768986cce6a3b31b05
p.grigoriev <p.grigoriev@bizon.help>
2019-11-17 19:27:14 +0300

    feature(watch): add movies list

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { App } from './components/app/app';
+import { moviesNames } from './mock/moviesNames';
 
 
-const init = () => {
+const init = (): void => {
   ReactDOM.render(
-    <App />,
+    <App movieNames={ moviesNames } />,
     document.querySelector('#root'),
   );
 };
---
+++ src/interfaces.ts
@@ -0,0 +1,7 @@
+export interface IMainScreenProps {
+  movieNames: string[];
+}
+
+export interface IAppProps extends IMainScreenProps {
+  movieNames: string[];
+}
